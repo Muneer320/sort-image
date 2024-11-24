7e70a1e0f3dedc4f54a4ab9002b5440a6adbeca6
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-11-24 18:59:02 +0530

    🌿 Update main()
    - Remove imports
    - Add imports from util and sort
    - Restructure main with util and sort

--- src/main.py
+++ src/main.py
@@ -1,148 +1,57 @@
-from os import mkdir, path
-from random import shuffle
 from shutil import rmtree
-from sys import argv, exit
 
-import ffmpeg
-from PIL import Image
+from rich import print as r_print
 from rich.console import Console
 
-split_size = 50
-total_splits = 0
-
-
-def sv_get_image():
-    if len(argv) < 2 or not path.exists(argv[1]):
-        return None
-
-    return Image.open(argv[1])
-
-
-def sv_random(max_range):
-    random_sequence = [i for i in range(max_range)]
-    shuffle(random_sequence)
-
-    return random_sequence
-
-
-def sv_get_w_h_splits(width, height):
-    return width // split_size, height // split_size
-
-
-def sv_split(image):
-    images = []
-
-    width, height = image.size
-    w_iter, h_iter = sv_get_w_h_splits(width, height)
-
-    left = 0
-    upper = 0
-    right = split_size
-    lower = split_size
-
-    counter = 0
-
-    for i in range(h_iter):
-        for j in range(w_iter):
-            temporary_image = image.crop((left, upper, right, lower))
-
-            images.append(temporary_image)
-            counter += 1
-
-            left += split_size
-            right += split_size
-
-        upper += split_size
-        lower += split_size
-
-        left = 0
-        right = split_size
-
-    return images
-
-
-def sv_merge(image, images, sequence, iteration):
-    width, height = image.size
-    w_iter, h_iter = sv_get_w_h_splits(width, height)
-
-    left = 0
-    upper = 0
-    right = split_size
-    lower = split_size
-
-    merge_image = Image.new("RGB", (width, height))
-
-    counter = 0
-
-    for i in range(h_iter):
-        for j in range(w_iter):
-            merge_image.paste(images[sequence[counter]], (left, upper, right, lower))
-
-            counter += 1
-
-            left += split_size
-            right += split_size
-
-        upper += split_size
-        lower += split_size
-
-        left = 0
-        right = split_size
-
-    merge_image.save(f"merge/{iteration:06}.jpg")
-
-
-def sv_bubble_sort(image, images, sequence):
-    n = len(sequence)
-
-    for i in range(n):
-        for j in range(n - 1):
-            if sequence[j] > sequence[j + 1]:
-                sequence[j], sequence[j + 1] = sequence[j + 1], sequence[j]
-
-        sv_merge(image, images, sequence, i)
-
-
-def sv_create_video():
-    ffmpeg.input("merge/*.jpg", pattern_type="glob", framerate=30).output(
-        "sort-image.mp4", loglevel="quiet"
-    ).run(overwrite_output=True)
+import util
+from sort import SVSort
 
 
 def main():
-    global total_splits, split_size
-
-    if len(argv) > 2:
-        split_size = int(argv[2])
-
     console = Console()
+    args = util.sv_parse_args()
 
-    image = sv_get_image()
+    directory = "sv"
+    video = "sort-image.mp4"
+
+    # Get image
+    image = util.sv_parse_image(args.image)
 
     if image is None:
-        exit(-1)
+        console.print("[bold red]Error[/bold red]: Invalid image file.")
+        exit(1)
 
-    width, height = image.size
+    # Get split size
+    split = int(args.split) if args.split else 50
 
-    total_splits = (width // split_size) * (height // split_size)
+    w, h = image.size
+    splits = (w // split) * (h // split)
 
-    if not path.exists("merge"):
-        mkdir("merge")
-    else:
-        rmtree("merge/", ignore_errors=True)
-        mkdir("merge")
+    util.sv_create_merge_dir()
 
-    with console.status("Splitting image into pieces...", spinner="dots9"):
-        images = sv_split(image)
+    # Process
+    status = console.status
 
-    with console.status("Sorting with bubble sort...", spinner="dots9"):
-        sv_bubble_sort(image, images, sv_random(total_splits))
+    with status("Splitting image into pieces...", spinner="dots9"):
+        images = util.sv_split_image(image, split)
 
-    with console.status("Creating video with ffmpeg...", spinner="dots9"):
-        sv_create_video()
+    with status("Sorting with bubble sort...", spinner="dots9"):
+        array = util.sv_generate_array(splits)
+        sort = SVSort(array)
 
-    rmtree("merge/", ignore_errors=True)
+        for index, iteration in enumerate(sort.bubble_sort()):
+            util.sv_merge_image(image, images, iteration, index, split)
+
+    with status("Creating video with ffmpeg...", spinner="dots9"):
+        util.sv_create_video(directory, video)
+
+    rmtree(directory, ignore_errors=True)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        rmtree("sv/", ignore_errors=True)
+        r_print("[bold red]Error[/bold red]: Exception occured.")
+        exit(1)
