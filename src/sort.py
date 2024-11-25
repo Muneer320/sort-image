e5d30480faad60171ae283147670bc1591083772
surajkareppagol <surajkareppagol.dev@gmail.com>
2024-11-25 22:49:20 +0530

    🌿 Add algorithms list and sort() method
    - Add algorithms list
    - Add all in one sort() method
    - Use algorithm index to return sorting algorithm

--- src/sort.py
+++ src/sort.py
@@ -6,13 +6,20 @@ class SVSort:
         self.array = array
         self.size = len(array)
 
+        self.algorithms: list[object] = []
+        self.algorithms.append(self.bubble_sort)
+
     def swap(self, i: int, j: int) -> None:
         self.array[i], self.array[j] = self.array[j], self.array[i]
 
-    def bubble_sort(self) -> Generator:
+    def bubble_sort(self) -> Generator[list[int], None, None]:
         for _ in range(self.size):
             for j in range(self.size - 1):
                 if self.array[j] > self.array[j + 1]:
                     self.swap(j, j + 1)
 
             yield self.array
+
+    def sort(self, index: int) -> object:
+        algorithm = self.algorithms[index]
+        return algorithm
