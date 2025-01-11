5db8dee637c39c10f1be4be3d64b32d6dad396d2
surajkareppagol <surajkareppagol.dev@gmail.com>
2025-01-12 00:12:51 +0530

    up: Refactor and modify some variables
    - Use builder argument
    - Pass image size as dimensions
    - Collect algorithm in Progress
    - If algorithm is invalid default to bubble_sort

--- src/main.py
+++ src/main.py
@@ -6,56 +6,60 @@ import util
 from sort import SVSort
 from term import r_print, status
 
+# *************************************************#
+#              SV_MAIN                             #
+# *************************************************#
 
-def main() -> None:
+
+def sv_main() -> None:
     args = util.sv_parse_args()
-
     directory = "sv"
 
-    # Get image
     image = util.sv_parse_image(args.image)
 
     if image is None:
         r_print("[bold red]Error[/bold red]: Invalid image file.")
         exit(1)
 
-    # Get split size
     split = int(args.split) if args.split else 50
-
-    # Algorithm
-    algorithm = int(args.algorithm) if args.algorithm else 0
-
-    # Video Formatter
-    formatter = args.video_formatter
+    builder = args.builder if args.builder else "ff"
 
     w, h = image.size
     splits = (w // split) * (h // split)
 
     util.sv_create_merge_dir()
 
-    # Process Image
     with status("Splitting image into pieces...", spinner="dots9"):
         images = util.sv_split_image(image, split)
 
     with Progress() as progress:
-        task = progress.add_task(
-            f"Generating {splits} images...", total=splits)
+        task = progress.add_task(f"Generating {splits} images...", total=splits)
 
         array = util.sv_generate_array(splits)
-        sorter = SVSort(array)
-        sort = sorter.sort(algorithm)
+        sv_sort = SVSort(array)
+
+        # Generator
+        algorithm = (
+            int(args.algorithm)
+            if (args.algorithm and int(args.algorithm) < sv_sort.total_algorithms)
+            else 0
+        )
+
+        sort = sv_sort.sort(algorithm)
 
         for index, iteration in enumerate(sort()):
             util.sv_merge_image(image, images, iteration, index, split)
             progress.update(task, advance=1)
 
-    video = [algo.__name__ for algo in sorter.algorithms][algorithm] + ".mp4"
+    video = f"{sv_sort.algorithms[algorithm].__name__}.mp4"
 
-    if formatter == "opencv":
-        util.sv_create_video_opencv(directory, video)
-    else:
+    # Defaults to FFMEPG
+    if builder == "ff":
         with status("Creating video with ffmpeg...", spinner="dots9"):
-            util.sv_create_video_ffmpeg(directory, video)
+            util.sv_create_video_ffmpeg(directory, video, image.size)
+    elif builder == "cv":
+        with status("Creating video with OpenCV...", spinner="dots9"):
+            util.sv_create_video_opencv(directory, video, image.size)
 
     rmtree(directory, ignore_errors=True)
 
@@ -64,10 +68,11 @@ def main() -> None:
 
 if __name__ == "__main__":
     try:
-        main()
-    except KeyboardInterrupt:
+        sv_main()
+    except KeyboardInterrupt as interrupt:
         rmtree("sv/", ignore_errors=True)
         r_print("[bold red]Error[/bold red]: Interrupted.")
+        r_print(interrupt)
         exit(1)
     except Exception as exception:
         rmtree("sv/", ignore_errors=True)
